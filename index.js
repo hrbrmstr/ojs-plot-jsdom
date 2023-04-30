ff6b5af29be65b3898c84f20472ba9bb787d082a
hrbrmstr <bob@rud.is>
2023-04-30 09:32:20 -0400

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import * as Plot from "@observablehq/plot"
+import { JSDOM } from "jsdom"
+
+const jsdom = new JSDOM(`<figure id='plt'></figure>`)
+
+const rociCrewCoffeConsumption = [
+	{ cups: 30, member: "Holden" },
+	{ cups: 5, member: "Amos" },
+	{ cups: 3, member: "Alex" },
+	{ cups: 1, member: "Naomi" },
+];
+
+const plt = jsdom.window.document.getElementById('plt')
+
+plt.appendChild(
+	Plot.plot({
+		document: jsdom.window.document,
+		marginLeft: 50,
+		style: {
+			background: "none",
+			color: "#4a6d88"
+		},
+		x: {
+			grid: true,
+			label: "Cups of ☕️ consumed, per-day"
+		},
+		y: {
+
+			label: null
+		},
+		marks: [
+			Plot.barX(rociCrewCoffeConsumption, { x: "cups", y: "member", sort: { y: "x", reverse: true} })
+		]
+	})
+);
+
+console.log(plt.innerHTML)
